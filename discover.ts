adb0c26df9a99db8f11e44b697003c21f4052eff
Douglas Gusson <douglasgusson@gmail.com>
2025-02-21 16:23:15 -0300

    chore: Add initial project files and dependencies

---
+++ discover.ts
@@ -0,0 +1,32 @@
+import pl from 'nodejs-polars';
+
+const extractedFile = Bun.file("./extracted-data-example.json");
+const extractedJson = await extractedFile.json();
+
+const df = pl.DataFrame(extractedJson);
+
+const authorsResult = df
+  .explode("authors")
+  .dropNulls("authors")
+  .groupBy("authors")
+  .len()
+  .sort("authors_count", true);
+
+const publisherResult = df
+  .dropNulls("publisher")
+  .groupBy("publisher")
+  .len()
+  .sort("publisher_count", true);
+
+const authorRef = "Rezende, Aldo";
+
+const aldoResendeArtifacts = df
+  .explode("authors")
+  .filter(pl.col("authors").eq(pl.lit(authorRef)))
+  .select("title", "publisher", pl.col("date").alias("year"))
+  .sort("year", true);
+
+console.debug(df.head());
+console.debug(publisherResult.head(10));
+console.debug(authorsResult.head(10));
+console.debug(aldoResendeArtifacts)
---
+++ index.ts
@@ -0,0 +1,72 @@
+import * as cheerio from 'cheerio';
+
+const fetchContent = async (url: string | URL) => {
+  console.debug(url.toString());
+  const response = await fetch(url);
+  return await response.text();
+}
+
+const extractItems = ($: cheerio.CheerioAPI) => {
+  const { items } = $.extract({
+    items: [
+      {
+        selector: ".artifact-description",
+        value: {
+          title: {
+            selector: ".artifact-title",
+            value: (el) => $(el).text().trim(),
+          },
+          path: {
+            selector: ".artifact-title > a",
+            value: "href",
+          },
+          abstract: ".artifact-abstract",
+          authors: [".artifact-info > .author span"],
+          publisher: ".publisher-date > .publisher",
+          date: ".publisher-date > .date",
+        },
+      },
+    ],
+  });
+
+  return items;
+}
+
+const main = async () => {
+  const url = new URL("https://repositorio.ifes.edu.br/recent-submissions");
+
+  let offset: string | undefined;
+
+  const now = new Date();
+  const filePath = `./data/extracted-${now.toJSON()}.json`
+  const file = Bun.file(filePath);
+  const writer = file.writer();
+
+  writer.write("[");
+
+  do {
+    const content = await fetchContent(url);
+    const $ = cheerio.load(content);
+
+    offset = $(".next-page-link").attr("href")?.replace(/\D/g, '');
+
+    const items = extractItems($).map(item => ({
+      ...item,
+      link: `${url.origin}${item.path}`,
+    }));
+
+    items.forEach((item, i) => {
+      writer.write(JSON.stringify(item));
+      const isLast = i === items.length - 1 && !offset;
+      if (!isLast) writer.write(",");
+    });
+
+    if (offset) url.searchParams.set("offset", offset);
+
+  } while (!!offset);
+
+  writer.write("]");
+  writer.end();
+}
+
+main()
